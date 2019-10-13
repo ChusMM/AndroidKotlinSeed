12b27c07007ee01c75e20ead4efd6b630206bd65
chuster <jm.munoz.maz@gmail.com>
2019-10-13 11:29:40 +0200

    Mock android Log

--- app/src/test/java/com/example/androidkotlinseed/repository/DataWebServiceUnitTest.kt
+++ app/src/test/java/com/example/androidkotlinseed/repository/DataWebServiceUnitTest.kt
@@ -20,6 +20,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.*
+import org.powermock.api.mockito.PowerMockito
 import org.powermock.core.classloader.annotations.PowerMockIgnore
 import org.powermock.core.classloader.annotations.PrepareForTest
 import org.powermock.modules.junit4.PowerMockRunner
@@ -53,6 +54,8 @@ class DataWebServiceUnitTest {
 
     @Before
     fun setup() {
+        PowerMockito.mockStatic(Log::class.java)
+
         mockWebServer = MockWebServer()
         mockWebServer.start(8080)
 
